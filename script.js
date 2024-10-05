cb6b6fad39c27295e026a34d9780648c993fe48c
Duck_Genus <genus.duck@gmail.com>
2024-10-05 22:43:31 +0530

    Added 3D visualization with the help of NASA's exoplanet data and minor tweaks

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const infoCollected = [
   {
-    name: "GLIESE 667CC",
-    type: "SuperEarth",
-    mass: "4.5 - 5",
-    radius: "1.5",
-    info: "Gliese 667 Cc is significant in the study of exoplanets because of its potential habitability and proximity to Earth. It serves as a target for further astronomical observations to understand more about its atmosphere and surface conditions.",
-    link: "https://www.solarsystemquick.com/universe/gliese-667cc",
-    img: "gliese-667cc-removebg-preview.png",
-    height: 200,
-    width: 260,
-  },
-  {
     name: "Kepler-22b",
     type: "SuperEarth",
     mass: "2.4 - 6.6",
@@ -20,6 +9,7 @@ const infoCollected = [
     img: "mainimage_Kepler22b_Planet-removebg-preview.png",
     height: 233,
     width: 280,
+    visualize: "https://eyes.nasa.gov/apps/exo/#/planet/Kepler-22_b",
   },
   {
     name: "KEPLER-62f",
@@ -31,10 +21,11 @@ const infoCollected = [
     img: "5748162991463-removebg-preview.png",
     height: 180,
     width: 280,
+    visualize: "https://eyes.nasa.gov/apps/exo/#/planet/Kepler-62_f",
   },
   {
     name: "PROXIMA CENTAURI B",
-    type: "Earth-like",
+    type: "Earthish",
     mass: "1.17",
     radius: "1.08",
     info: "Proxima Centauri b is particularly significant because it is the closest potentially habitable exoplanet to Earth, making it a prime target for future studies and observations in the search for extraterrestrial life. Its proximity offers unique opportunities for exploration and potential future missions.",
@@ -42,17 +33,31 @@ const infoCollected = [
     img: "Proxima-Centauri-b-removebg-preview.png",
     height: 180,
     width: 250,
+    visualize: "https://eyes.nasa.gov/apps/exo/#/planet/Proxima_Cen_b",
+  },
+  {
+    name: "GLIESE 667Cc",
+    type: "SuperEarth",
+    mass: "3.8",
+    radius: "1.77",
+    info: "Gliese 667 Cc is significant in the study of exoplanets because of its potential habitability and proximity to Earth. It serves as a target for further astronomical observations to understand more about its atmosphere and surface conditions.",
+    link: "https://en.wikipedia.org/wiki/Gliese_667_Cc",
+    img: "gliese-667cc-removebg-preview.png",
+    height: 200,
+    width: 260,
+    visualize: "https://eyes.nasa.gov/apps/exo/#/planet/GJ_667_C_c",
   },
   {
     name: "TRAPPIST-1e",
-    type: "Earth-like",
+    type: "Earthish",
     mass: "0.77",
     radius: "0.91",
-    info: "The planet is one of the most Earth-like worlds found. It is extremely similar to Earth physically, with 91% the radius, 77% the mass, 102.4% the density (5.65 g/cm3), and 93% the surface gravity of Earth. TRAPPIST-1e is confirmed to be a terrestrial planet with a solid, rocky surface. It is cool enough for liquid water to pool on the surface, but not too cold for it to freeze like on TRAPPIST-1f, g, and h. The planet receives a stellar flux 0.604 times that of Earth, about a third lower than that of Earth but significantly more than that of Mars. ",
+    info: "TRAPPIST-1e is a promising exoplanet in the search for habitability, closely resembling Earth with 91% of its radius and 77% of its mass. It has a solid, rocky surface, similar density, and surface gravity at 93% of Earth's. The planet's cooler temperatures allow for liquid water, while receiving about 60% of Earth's stellar flux. These factors position TRAPPIST-1e as a key target for future studies on its atmosphere and potential for life. (analyzed by AI)",
     link: "https://astronomical.fandom.com/wiki/TRAPPIST-1e",
     img: "TRAPPIST-1e_2018_impression-removebg-preview.png",
     height: 200,
     width: 200,
+    visualize: "https://eyes.nasa.gov/apps/exo/#/planet/TRAPPIST-1_ez",
   },
 ];
 
@@ -68,9 +73,10 @@ infoCollected.forEach((element) => {
         <div>
             <span class="p_mass"><b>Mass: </b>${element.mass} times earth</span>
             <span class="p_radius"><b>Radius: </b>${element.radius} times earth</span>
-            <p>${element.info} <br> <a href="${element.link}">Learn more</a>
+            <p>${element.info} 
+              <br> <a href="${element.link}">Learn more</a>
+              <br> <a href="${element.visualize}" style="margin-top: 1rem">Visualize in 3D by NASA's Data</a>
             </p>
-        </p>
         </div>
     </div>
     <img
